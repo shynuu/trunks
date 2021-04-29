c1138878755785f146d979f8990e2c66ade97905
Youssouf Drif <drif.youssouf@protonmail.ch>
2021-04-29 11:08:16 +0200

    Update client and server to UDP

--- container/client/server.sh
+++ container/client/server.sh
@@ -2,5 +2,5 @@
 
 ip route add 10.100.200.0/24 via 10.0.1.2
 
-iperf -s -B 10.0.1.10 -p 3000 --tos 0xB8 & 
-iperf -s -B 10.0.1.10 -p 2500 &
+iperf -s -B 10.0.1.10 -p 3000 --tos 0xB8 -u -l 128 &
+iperf -s -B 10.0.1.10 -p 2500 -u -l 1450 &
